5c4aed8fec33bc07df3634efe58d98ea102918c6
Leon Breedt <leon@sector42.io>
2023-05-08 21:49:31 +1200

    fix: remove unused file

--- macos/preferences.nix
+++
@@ -1,9 +0,0 @@
-# macOS specific packages
-
-{ lib, pkgs, machineConfig }:
-
-with pkgs;
-let common-packages = import ../common/packages.nix { lib = lib; pkgs = pkgs; machineConfig = machineConfig; }; in
-common-packages ++ [
-  dockutil
-]
