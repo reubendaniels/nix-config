81b18b94c2ce62e9267b52b7b3cc9e94b05c73c7
Leon Breedt <leon.breedt@sap.com>
2024-02-13 13:43:15 +1300

    chore: update chromedriver

--- overlays/chromedriver-latest.nix
+++ overlays/chromedriver-latest.nix
@@ -1,5 +1,5 @@
 let 
-  chromePkgs = import (fetchTarball https://github.com/nixos/nixpkgs/archive/8f824c9d1315fb0a3a74277b64d24c632f6a92d2.tar.gz) {};
+  chromePkgs = import (fetchTarball https://github.com/nixos/nixpkgs/archive/897df4cc4d21cefc2b3b298fa75b5a1ab500ebd5.tar.gz) {};
 in
 self: super: {
   chromedriver-latest = chromePkgs.chromedriver;
