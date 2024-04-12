3b1540001bc3ef257608f5456700f8463725f90d
Leon Breedt <leon@sector42.io>
2024-04-12 21:21:03 +1200

    chore: remove Safari as default browser
    too flaky, the new IE6

--- macos/dock.nix
+++ macos/dock.nix
@@ -2,9 +2,11 @@
 {pkgs, lib, isPersonal}:
 
 [
-  { path = "/System/Cryptexes/App/System/Applications/Safari.app/"; }
   { path = "${pkgs.wezterm}/Applications/WezTerm.app/"; }
+] ++ lib.optionals (!isPersonal) [
+  { path = "/Applications/Google Chrome.app/"; }
 ] ++ lib.optionals isPersonal [
+  { path = "/Applications/Brave Browser.app/"; }
   { path = "/System/Applications/Messages.app/"; }
   { path = "/System/Applications/Photos.app/"; }
   { path = "/Applications/Mimestream.app/"; }
