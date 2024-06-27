b8fd937aef998dba0a24d1040a40d376af8b0c01
Leon Breedt <leon@sector42.io>
2024-06-27 20:47:36 +1200

    fix: put /run/wrappers/bin at the start of PATH

--- common/home.nix
+++ common/home.nix
@@ -31,7 +31,7 @@
     fish = {
       enable = true;
       shellInit = ''
-        fish_add_path /run/wrappers/bin
+        fish_add_path --prepend /run/wrappers/bin
         fish_add_path /run/current-system/sw/bin
         if test -d $HOME/.cargo/bin
           fish_add_path $HOME/.cargo/bin
