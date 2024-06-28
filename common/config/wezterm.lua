6aabff012701c16a2bbcf06a3aa61680ab673839
Leon Breedt <leon@sector42.io>
2024-06-28 22:05:13 +1200

    fix: adjust window decorations on Linux

--- common/config/wezterm.lua
+++ common/config/wezterm.lua
@@ -5,32 +5,14 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- fonts
+local is_linux = function()
+	return wezterm.target_triple:find("linux") ~= nil
+end
 
-config.font = wezterm.font("IosevkaLB", {weight="DemiBold"})
-config.font_size = 12.0
-config.font_rules = {
-  {
-    intensity = "Bold",
-    font = wezterm.font("IosevkaLB", {weight="Bold"})
-  }
-}
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-config.initial_cols = 110
-config.initial_rows = 35
+local is_macos = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
 
--- window configuration
-
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.window_padding = {
-  left = 15,
-  right = 15,
-  top = 50,
-  bottom = 10,
-}
-
--- wezterm.gui is not available to the mux server, so take care to
--- do something reasonable when this config is evaluated by the mux
 function get_appearance()
   if wezterm.gui then
     return wezterm.gui.get_appearance()
@@ -46,55 +28,38 @@ function scheme_for_appearance(appearance)
   end
 end
 
+function get_window_decorations()
+  if is_macos() then
+    return 'RESIZE'
+  else
+    return 'INTEGRATED_BUTTONS|RESIZE'
+  end
+end
+
+-- fonts
+
+config.font = wezterm.font("IosevkaLB", {weight="DemiBold"})
+config.font_size = 12.0
+config.font_rules = {
+  {
+    intensity = "Bold",
+    font = wezterm.font("IosevkaLB", {weight="Bold"})
+  }
+}
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.initial_cols = 110
+config.initial_rows = 35
+
+-- window configuration
+config.window_decorations = get_window_decorations()
+config.window_padding = {
+  left = 15,
+  right = 15,
+  top = 50,
+  bottom = 10,
+}
+
 config.color_scheme = scheme_for_appearance(get_appearance())
-
--- window frame colors
---config.window_frame = {
---  inactive_titlebar_bg = '#282c34',
---  active_titlebar_bg = '#282c34',
---  inactive_titlebar_fg = '#cccccc',
---  active_titlebar_fg = '#ffffff',
---  inactive_titlebar_border_bottom = '#2b2042',
---  active_titlebar_border_bottom = '#2b2042',
---  button_fg = '#cccccc',
---  button_bg = '#2b2042',
---  button_hover_fg = '#ffffff',
---  button_hover_bg = '#3b3052',
---}
-
--- window content colors
---config.colors = {
---  foreground = '#dcdfe4',
---  background = '#282c34',
-
---  selection_fg = '#000000',
---  selection_bg = '#fffacd',
-
- -- ansi = {
- --   '#5d677a',
- --   '#e06c75',
- --   '#98c379',
- --   '#e5c07b',
- --   '#61afef',
- --   '#c678dd',
- --   '#56b6c2',
- --   '#dcdfe4',
- -- },
- -- brights = {
- --   '#5d677a',
- --   '#e06c75',
- --   '#98c379',
- --   '#e5c07b',
- --   '#61afef',
- --   '#c678dd',
- --   '#56b6c2',
- --   '#dcdfe4',
- -- },
---
---  cursor_bg = '#52ad70',
---  cursor_fg = 'black',
---  cursor_border = '#52ad70',
---}
 
 -- tab bar
 config.hide_tab_bar_if_only_one_tab = true
@@ -104,6 +69,9 @@ config.tab_bar_at_bottom = true
 config.default_cursor_style = 'SteadyBar'
 config.cursor_thickness = '3px'
 
-config.enable_wayland = false
+-- disable wayland on Linux
+if is_linux() then
+  config.enable_wayland = false
+end
 
 return config
